1dc6d8cdda1671622dec7a2a79b3a6539e1efe7b
natesheridan <nbs@dr.com>
2022-06-04 23:34:28 -0600

    Add Basic CSS and APP Root HTML

--- index.js
+++ index.js
@@ -3,4 +3,49 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Nate Starter</h1>`;
+appDiv.innerHTML = `
+<div class="card">
+  <h2>Form Typechecker and Formatter</h2>
+  <div class="row">
+    <div class="col">
+      <div class="form-group">
+        <label>First Name</label>
+        <input type="text">
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="form-group">
+        <label>Surname</label>
+        <input type="text">
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="form-group">
+        <label>Email</label>
+        <input type="text">
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="form-group">
+        <label>Phone</label>
+        <input type="text">
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="form-group">
+        <label>Message</label>
+        <textarea></textarea>
+      </div>
+    </div>
+
+    <div class="col">
+      <input type="submit" value="Submit">
+    </div>
+  </div>
+</div`;
+
+
